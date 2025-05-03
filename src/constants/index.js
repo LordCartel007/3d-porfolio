3f167262c073d81c2e774551462fafb613544288
LordCartel007 <cartellord77@gmail.com>
2025-05-03 22:14:06 +0100

    added 3d logos

--- src/constants/index.js
+++ src/constants/index.js
@@ -111,7 +111,7 @@ export const abilities = [
 export const expCards = [
   {
     review:
-      "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
+      "Casey brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
     imgPath: "/images/exp1.png",
     logoPath: "/images/logo1.png",
     title: "Frontend Developer",
@@ -124,7 +124,7 @@ export const expCards = [
   },
   {
     review:
-      "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
+      "Casey’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
     imgPath: "/images/exp2.png",
     logoPath: "/images/logo2.png",
     title: "Full Stack Developer",
@@ -137,7 +137,7 @@ export const expCards = [
   },
   {
     review:
-      "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
+      "Casey’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
     imgPath: "/images/exp3.png",
     logoPath: "/images/logo3.png",
     title: "React Native Developer",
@@ -147,5 +147,61 @@ export const expCards = [
       "Improved app performance and user experience through code optimization and testing.",
       "Coordinated with the product team to implement features based on feedback.",
     ],
+  },
+];
+
+export const techStackIcons = [
+  {
+    name: "React Developer",
+    modelPath: "/models/react_logo-transformed.glb",
+    scale: 1,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Python Developer",
+    modelPath: "/models/python-transformed.glb",
+    scale: 0.8,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Backend Developer",
+    modelPath: "/models/node-transformed.glb",
+    scale: 5,
+    rotation: [0, -Math.PI / 2, 0],
+  },
+  {
+    name: "Interactive Developer",
+    modelPath: "/models/three.js-transformed.glb",
+    scale: 0.05,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Project Manager",
+    modelPath: "/models/git-svg-transformed.glb",
+    scale: 0.05,
+    rotation: [0, -Math.PI / 4, 0],
+  },
+];
+
+export const techStackImgs = [
+  {
+    name: "React Developer",
+    imgPath: "/images/logos/react.png",
+  },
+  {
+    name: "Python Developer",
+    imgPath: "/images/logos/python.svg",
+  },
+  {
+    name: "Backend Developer",
+    imgPath: "/images/logos/node.png",
+  },
+  {
+    name: "Interactive Developer",
+    imgPath: "/images/logos/three.png",
+  },
+  {
+    name: "Project Manager",
+    imgPath: "/images/logos/git.svg",
   },
 ];
---
+++ src/sections/TechStack.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import TitleHeader from "../components/TitleHeader";
+import TechIcon from "../components/Models/TechLogos/TechIcon";
+import { techStackIcons } from "../constants/index.js";
+
+const TechStack = () => {
+  return (
+    <div id="skills" className="flex-center section-padding">
+      <div className="w-full h-full md:px-10 px-5">
+        <TitleHeader
+          title="My Preferred Tech Stack"
+          sub="The Skills I Bring to the Table"
+        />
+
+        <div className="tech-grid">
+          {/* Loop through the techStackIcons array and create a component for each item. 
+              The key is set to the name of the tech stack icon, and the classnames are set to 
+              card-border, tech-card, overflow-hidden, and group. The xl:rounded-full and rounded-lg 
+              classes are only applied on larger screens. */}
+          {techStackIcons.map((icon) => (
+            <div
+              key={icon.name}
+              className="card-border tech-card
+            overflow-hidden group xl:rounded-full rounded-lg"
+            >
+              {" "}
+              {/* The tech-card-animated-bg div is used to create a background animation when the 
+                component is hovered. */}
+              <div className="tech-card-animated-bg" />
+              <div className="tech-card-content">
+                <div className="tech-icon-wrapper">
+                  <TechIcon model={icon} />
+                </div>
+
+                <div className="padding-x w-full">
+                  <p>{icon.name}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TechStack;
